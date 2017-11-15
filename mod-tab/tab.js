46470b1122824a045b1a850350e8bd7cbd06a94d
李奇男 <18347255592@163.com>
2017-11-15 23:01:57 +0800

    add tab

---
+++ mod-tab/tab.js
@@ -0,0 +1,30 @@
+//jQuery practice
+$('.mod-tab .tab').on('click', function(){
+	$(this).addClass('active').siblings().removeClass('active');
+	var index = $(this).index();
+	$(this).parents('.mod-tab').find('.panel').eq(index).addClass('active').siblings().removeClass('active');
+});
+
+
+//nativeJS practice
+
+// document.querySelectorAll('.mod-tab .tab').forEach(function(node){
+// 	    node.addEventListener('click',function(){
+// 		var index;
+// 		this.parentElement.querySelectorAll('.tab').forEach(function(tab,idx){
+// 			tab.classList.remove('active');
+// 			if(node === tab){
+// 				index = idx;
+// 			}
+// 		});
+// 			this.classList.add('active');
+
+// 			this.parentElement.nextElementSibling.querySelectorAll('.panel').forEach(function(panel){
+//                  panel.classList.remove('active');
+// 			});
+// 			this.parentElement.nextElementSibling.querySelectorAll('.panel')[index].classList.add('active');
+
+		
+
+// 	});
+// })
